462afa82a152204dae91d10df22d24d13f3b56f2
unknown <prakharvision@gmail.com>
2025-02-25 16:25:30 +0530

    new changes has been made

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Roboto,Lusitana } from 'next/font/google';
+import { Roboto,Lusitana ,Inter} from 'next/font/google';
 
 export const roboto = Roboto({ subsets: ['latin'] ,weight:'400'});
 export const lusitana=Lusitana({subsets:['latin'],weight:'400'});
+export const inter=Inter({subsets:['latin']});
