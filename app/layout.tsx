a60275c55fd780940be347b44e23e27cf9f38756
unknown <prakharvision@gmail.com>
2025-02-25 16:32:23 +0530

    this is tutorial for the next js from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import '@/app/ui/global.css';
-import {Inter} from "next/font/google";
-export const inter = Inter({ subsets: ['latin'] });
 import {roboto} from '@/app/ui/fonts';
 export default function RootLayout({
   children,
